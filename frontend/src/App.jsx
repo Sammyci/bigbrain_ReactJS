42e23c9d8fb85deeec1c72cfaa9b3a750458948f
Sixing Ci <s.ci@student.unsw.edu.au>
2023-04-19 00:21:53 +1000

    Replace App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import Wrapper from './Wrapper';
 
 function App () {
   return (
-    <>Let&apos;s go!</>
+    <Router>
+      <Wrapper />
+    </Router>
   );
 }
 
